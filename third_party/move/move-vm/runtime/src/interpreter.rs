fb2cfae94d97a6c6be4938b3ac324a8473e46a7a
Poytr1 <pooytr1@gmail.com>
2024-03-03 11:13:37 +0800

    Fix the wrong type arguments when meeting a call

--- third_party/move/move-vm/runtime/src/interpreter.rs
+++ third_party/move/move-vm/runtime/src/interpreter.rs
@@ -621,9 +621,7 @@ impl Interpreter {
                             }
                         }).map(|v: Result<MoveValue, PartialVMError>| v.unwrap_or(MoveValue::U8(0))).collect(),
                         outputs: vec![],
-                        type_args: current_frame.ty_args().into_iter().map(|ty| {
-                            loader.type_to_type_tag(ty).unwrap().to_string()
-                        }).collect(),
+                        type_args: vec![],
                         sub_traces: CallTraces::new(),
                     }).map_err(|_e| {
                         let err = PartialVMError::new(StatusCode::ABORTED);
@@ -717,7 +715,7 @@ impl Interpreter {
                             }
                         }).map(|v: Result<MoveValue, PartialVMError>| v.unwrap_or(MoveValue::U8(0))).collect(),
                         outputs: vec![],
-                        type_args: current_frame.ty_args().into_iter().map(|ty| {
+                        type_args: ty_args.iter().map(|ty| {
                             loader.type_to_type_tag(ty).unwrap().to_string()
                         }).collect(),
                         sub_traces: CallTraces::new(),
