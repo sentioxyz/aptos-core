c3ef6887b7f2f6e53aaea647394c43574574a612
Poytr1 <pooytr1@gmail.com>
2024-04-16 21:08:48 +0800

    Add support for multisig transaction

--- aptos-move/aptos-tracer/src/server.rs
+++ aptos-move/aptos-tracer/src/server.rs
@@ -2,19 +2,19 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::{CallTraceWithSource, DebuggerServerConfig, SyncAptosTracer};
-use poem::{
-    handler, http::Method, listener::TcpListener, middleware::Cors, EndpointExt, Route, Server, get, web::Path,
-};
+use poem::{handler, http::Method, listener::TcpListener, middleware::Cors, EndpointExt, Route, Server, get, web::Path, IntoResponse, Response};
 use anyhow::Result;
+use poem::error::ResponseError;
 use poem::middleware::AddData;
 use poem::web::{Data, Json};
+use reqwest::StatusCode;
 use serde_json;
 use url::Url;
-use aptos_logger::info;
+use aptos_logger::{error, info};
 use aptos_rest_client::Client;
 
 #[handler]
-async fn call_trace(Path((chain_id, hash)): Path<(u8, String)>, config: Data<&DebuggerServerConfig>) -> Json<CallTraceWithSource> {
+async fn call_trace(Path((chain_id, hash)): Path<(u8, String)>, config: Data<&DebuggerServerConfig>) -> Result<Json<CallTraceWithSource>> {
     let mut tracer;
     if config.use_db {
         tracer = SyncAptosTracer::db(config.db_path.to_str().unwrap(), config.clone().sentio_endpoint);
@@ -25,11 +25,21 @@ async fn call_trace(Path((chain_id, hash)): Path<(u8, String)>, config: Data<&De
     }
 
     let call_trace = if let Some(hex) = hash.strip_prefix("0x") {
-        tracer.unwrap().trace_transaction(hex.to_string(), chain_id).unwrap()
+        tracer.unwrap().trace_transaction(hex.to_string(), chain_id)
     } else {
-        tracer.unwrap().trace_transaction(hash, chain_id).unwrap()
+        tracer.unwrap().trace_transaction(hash, chain_id)
     };
-    return Json(call_trace);
+    match call_trace {
+        Ok(trace_result) => {
+            Ok(Json(trace_result))
+        }
+        Err(err) => {
+            error!("Error: {:?}", err);
+            Err(CustomError {
+                message: format!("{:?}", err),
+            }.into())
+        }
+    }
 }
 
 pub async fn run_debugger_server(
@@ -42,6 +52,22 @@ pub async fn run_debugger_server(
         config.clone().listen_address,
         config.listen_port,
     )))
-        .run(Route::new().at("/:chain_id/call_trace/by_hash/:hash", get(call_trace)).with(cors).with(AddData::new(config))).await
+        .run(
+            Route::new()
+                .at("/:chain_id/call_trace/by_hash/:hash", get(call_trace))
+                .with(cors)
+                .with(AddData::new(config))).await
         .map_err(anyhow::Error::msg)
+}
+
+#[derive(Debug, thiserror::Error)]
+#[error("{message}")]
+struct CustomError {
+    message: String,
+}
+
+impl ResponseError for CustomError {
+    fn status(&self) -> StatusCode {
+        StatusCode::BAD_REQUEST
+    }
 }
