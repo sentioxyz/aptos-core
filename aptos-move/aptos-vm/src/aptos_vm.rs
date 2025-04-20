330bfb95764ce571e759fe0ea3ae91c760b74713
lwedge99 <116623885+lwedge99@users.noreply.github.com>
2025-04-20 16:02:49 +0800

    resolve conflicts

--- aptos-move/aptos-vm/src/aptos_vm.rs
+++ aptos-move/aptos-vm/src/aptos_vm.rs
@@ -2608,7 +2608,7 @@ impl AptosVM {
                     txn_metadata.fee_payer().as_ref().map(|addr| serialized_signer(addr)),
                 );
 
-                let args = verifier::transaction_arg_validation::validate_combine_signer_and_txn_args(
+                let args = transaction_arg_validation::validate_combine_signer_and_txn_args(
                     &mut session,
                     &module_storage,
                     &serialized_signers,
@@ -2670,7 +2670,7 @@ impl AptosVM {
                     .collect();
                 let serialized_signers = SerializedSigners::new(senders_serialized, None);
 
-                let args = verifier::transaction_arg_validation::validate_combine_signer_and_txn_args(
+                let args = transaction_arg_validation::validate_combine_signer_and_txn_args(
                     &mut session,
                     &module_storage,
                     &serialized_signers,
