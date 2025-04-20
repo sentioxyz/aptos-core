1df72518fbf2e9688d042b7c12696be96ba6c829
lwedge99 <116623885+lwedge99@users.noreply.github.com>
2025-04-20 16:02:49 +0800

    resolve conflicts

--- aptos-move/aptos-tracer/src/sync_storage_interface.rs
+++ aptos-move/aptos-tracer/src/sync_storage_interface.rs
@@ -114,7 +114,7 @@ impl AptosTracerInterface for DBTracerInterface {
     ) -> Result<Option<Version>> {
         let ledger_version = self.get_latest_ledger_info_version()?;
         self.0
-            .get_account_transaction(account, seq, false, ledger_version)
+            .get_account_ordered_transaction(account, seq, false, ledger_version)
             .map_or_else(
                 |e| Err(anyhow::Error::from(e)),
                 |tp| Ok(tp.map(|e| e.version)),
