dd858adf91da8bf01be1994fb1360c412418d6c4
lwedge99 <116623885+lwedge99@users.noreply.github.com>
2025-05-11 20:56:37 +0800

    propagate error to upper levels

--- aptos-move/aptos-vm/src/aptos_vm.rs
+++ aptos-move/aptos-vm/src/aptos_vm.rs
@@ -2863,6 +2863,7 @@ impl AptosVM {
                     }
                 }
             }
+            TransactionPayload::Payload(_) => todo!()
         };
 
         let change_set_configs = storage_gas_params.change_set_configs;
